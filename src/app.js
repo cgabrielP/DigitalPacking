e0db628daa4f0b5fdbda74c37d6f42d99c7fbc2a
Carlos Peña <cgabriel2303@gmail.com>
2026-02-12 15:07:50 -0300

    update handle callback

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
+app.post("/webhooks/mercadolibre", (req, res) => {
+  console.log("Notificación recibida:", req.body);
+  res.sendStatus(200);
+});
+
 
 app.use("/auth", authRoutes);
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const redirectToML = (req, res) => {
   const url = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${process.env.ML_CLIENT_ID}&redirect_uri=${process.env.ML_REDIRECT_URI}`;
 
@@ -11,9 +13,35 @@ export const handleCallback = async (req, res) => {
     return res.status(400).json({ error: "No authorization code received" });
   }
 
-  // Por ahora solo mostramos el code
-  res.json({
-    message: "Authorization code recibido correctamente",
-    code
-  });
+  try {
+    const response = await axios.post(
+      "https://api.mercadolibre.com/oauth/token",
+      {
+        grant_type: "authorization_code",
+        client_id: process.env.ML_CLIENT_ID,
+        client_secret: process.env.ML_CLIENT_SECRET,
+        code,
+        redirect_uri: process.env.ML_REDIRECT_URI
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    const { access_token, refresh_token, user_id, expires_in } = response.data;
+
+    res.json({
+      message: "Cuenta conectada correctamente ✅",
+      access_token,
+      refresh_token,
+      user_id,
+      expires_in
+    });
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ error: "Error obteniendo access token" });
+  }
 };
